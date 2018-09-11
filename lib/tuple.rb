5ea6ac62068a92db02942dd3c38ce640c24c00c2
Sean McCarthy <sean.mccarthy@fluidintel.com>
2018-09-11 18:10:35 +1000

    Rest of tuple functions for Chapter 1

--- lib/tuple.rb
+++ lib/tuple.rb
@@ -23,12 +23,63 @@ class Tuple
   def -(other)
     Tuple.new(x - other.x, y - other.y, z - other.z, w - other.w)
   end
+
+  def -@
+    Tuple.new(-x, -y, -z, -w)
+  end
+
+  def *(other)
+    scalar = Float(other)
+    Tuple.new(x * scalar, y * scalar, z * scalar, w * scalar)
+  end
+
+  def /(other)
+    scalar = Float(other)
+    Tuple.new(x / scalar, y / scalar, z / scalar, w / scalar)
+  end
+
+  def magnitude
+    Math.sqrt(x**2 + y**2 + z**2 + w**2)
+  end
+
+  def normalise
+    denominator = magnitude
+    Tuple.new((x / denominator).round(EPSILON),
+              (y / denominator).round(EPSILON),
+              (z / denominator).round(EPSILON),
+              (w / denominator).round(EPSILON))
+  end
+
+  def dot(other)
+    (x * other.x) +
+      (y * other.y) +
+      (z * other.z) +
+      (w * other.w)
+  end
+
+  def cross(other)
+    Vector(y * other.z - z * other.y,
+           z * other.x - x * other.z,
+           x * other.y - y * other.x)
+  end
+end
+
+def Tuple(*args)
+  Tuple.new(*args)
+end
+
+def Point(*args)
+  Point.new(*args)
 end
 
 class Point < Tuple
   def initialize(x, y, z)
     super(x, y, z, 1)
   end
+end
+
+def Vector(*args)
+  Vector.new(*args)
 end
 
 class Vector < Tuple
--- test/tuple_test.rb
+++ test/tuple_test.rb
@@ -3,24 +3,83 @@ require_relative '../lib/tuple'
 
 class TestTuple < Minitest::Test
   def test_tuple_with_non_zero_w_is_a_point
-    tuple = Tuple.new(4.3, -4.2, 3.1, 1.0)
-    assert_equal tuple, Point.new(4.3, -4.2, 3.1)
+    tuple = Tuple(4.3, -4.2, 3.1, 1.0)
+    assert_equal tuple, Point(4.3, -4.2, 3.1)
   end
 
   def test_tuple_with_zero_w_is_a_vector
-    tuple = Tuple.new(4.3, -4.2, 3.1, 0)
-    assert_equal tuple, Vector.new(4.3, -4.2, 3.1)
+    tuple = Tuple(4.3, -4.2, 3.1, 0)
+    assert_equal tuple, Vector(4.3, -4.2, 3.1)
   end
 
   def test_add_tuples
-    tuple1 = Tuple.new(3, -2, 5, 1)
-    tuple2 = Tuple.new(-2, 3, 1, 0)
-    assert_equal tuple1 + tuple2, Tuple.new(1, 1, 6, 1)
+    tuple1 = Tuple(3, -2, 5, 1)
+    tuple2 = Tuple(-2, 3, 1, 0)
+    assert_equal tuple1 + tuple2, Tuple(1, 1, 6, 1)
   end
 
   def test_subtracting_points_gives_a_vector
-    p1 = Point.new(3, 2, 1)
-    p2 = Point.new(5, 6, 7)
-    assert_equal p1 - p2, Vector.new(-2, -4, -6)
+    p1 = Point(3, 2, 1)
+    p2 = Point(5, 6, 7)
+    assert_equal p1 - p2, Vector(-2, -4, -6)
+  end
+
+  def test_substracting_a_vector_from_a_point
+    p = Point(3, 2, 1)
+    v = Vector(5, 6, 7)
+    assert_equal p - v, Point(-2, -4, -6)
+  end
+
+  def test_subtracting_two_vectors
+    v1 = Vector(3, 2, 1)
+    v2 = Vector(5, 6, 7)
+    assert_equal v1 - v2, Vector(-2, -4, -6)
+  end
+
+  def test_negating_a_vector
+    t = Tuple(1, -2, 3, -4)
+    assert_equal(-t, Tuple(-1, 2, -3, 4))
+  end
+
+  def test_multiplying_a_tuple_by_a_scalar
+    t = Tuple(1, -2, 3, -4)
+    assert_equal t * 3.5, Tuple(3.5, -7, 10.5, -14)
+  end
+
+  def test_dividing_a_tuple_by_a_scalar
+    t = Tuple(1, -2, 3, -4)
+    assert_equal t / 2, Tuple(0.5, -1, 1.5, -2)
+  end
+
+  def test_magnitude_of_a_vector
+    v1 = Vector(1, 2, 3)
+    v2 = Vector(-1, -2, -3)
+    assert_equal v1.magnitude.round(Tuple::EPSILON),
+                 Math.sqrt(14).round(Tuple::EPSILON)
+    assert_equal v2.magnitude.round(Tuple::EPSILON),
+                 Math.sqrt(14).round(Tuple::EPSILON)
+  end
+
+  def test_normalise_a_vector
+    v = Vector(1, 2, 3)
+    assert_equal v.normalise, Vector(0.26726, 0.53452, 0.80178)
+  end
+
+  def test_magnitude_of_normalised_vector
+    v = Vector(1, 2, 3)
+    assert_equal v.normalise.magnitude.round(Tuple::EPSILON), 1
+  end
+
+  def test_dot_product_of_two_vectors
+    v1 = Vector(1, 2, 3)
+    v2 = Vector(2, 3, 4)
+    assert_equal v1.dot(v2), 20
+  end
+
+  def test_cross_product_of_two_vectors
+    v1 = Vector(1, 2, 3)
+    v2 = Vector(2, 3, 4)
+    assert_equal v1.cross(v2), Vector(-1, 2, -1)
+    assert_equal v2.cross(v1), Vector(1, -2, 1)
   end
 end

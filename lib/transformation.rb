82132d8930b8b9fe7b6d972cbb27d3fafc744b91
Sean McCarthy <sean.mccarthy@fluidintel.com>
2018-09-20 16:48:25 +1000

    Chapter 4 - scaling

--- lib/transformation.rb
+++ lib/transformation.rb
@@ -5,3 +5,11 @@ def Translation(x, y, z)
     m[2, 3] = z
   end
 end
+
+def Scaling(x, y, z)
+  Matrix.identity(4).tap do |m|
+    m[0, 0] = x
+    m[1, 1] = y
+    m[2, 2] = z
+  end
+end
--- test/transformations_test.rb
+++ test/transformations_test.rb
@@ -1,6 +1,6 @@
 require 'minitest/autorun'
 require_relative '../lib/tuple'
-require_relative '../lib/translation'
+require_relative '../lib/transformation'
 
 class TestTransformations < Minitest::Test
   def test_multiplying_by_translation_matrix
@@ -20,5 +20,34 @@ class TestTransformations < Minitest::Test
     transform = Translation(5, -3, 2)
     v = Vector(-3, 4, 5)
     assert_equal transform * v, v
+  end
+
+  def test_scaling_a_point
+    transform = Scaling(2, 3, 4)
+    p = Point(-4, 6, 8)
+    assert_equal transform * p, Point(-8, 18, 32)
+  end
+
+  def test_scaling_a_vector
+    transform = Scaling(2, 3, 4)
+    v = Vector(-4, 6, 8)
+    assert_equal transform * v, Vector(-8, 18, 32)
+  end
+
+  def test_multiply_by_inverse_of_scaling_matrix
+    transform = Scaling(2, 3, 4)
+    inv = transform.inverse
+    v = Vector(-4, 6, 8)
+    v_inverse = inv * v
+    assert_equal Vector(v_inverse.x.round(0),
+                        v_inverse.y.round(0),
+                        v_inverse.z.round(0)),
+                 Vector(-2, 2, 2)
+  end
+
+  def test_reflection_by_scaling_with_negative
+    transform = Scaling(-1, 1, 1)
+    p = Point(2, 3, 4)
+    assert_equal transform * p, Point(-2, 3, 4)
   end
 end

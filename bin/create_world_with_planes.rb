bee40ac8e312f2eedfbeea8895a8b849be255da9
Sean McCarthy <sean.mccarthy@fluidintel.com>
2019-08-10 20:52:27 +1000

    Updating create world script

--- bin/create_world_with_planes.rb
+++ bin/create_world_with_planes.rb
@@ -19,7 +19,7 @@ require_relative '../lib/checker_pattern'
 require_relative '../lib/blended_pattern'
 require 'benchmark'
 
-multiplier = 3
+multiplier = 4
 width = 200 * multiplier
 height = 100 * multiplier
 
@@ -27,84 +27,58 @@ def pick_colour
   rand(0.4...1.0).round(1)
 end
 
+vertical = StripePattern(a: Colour(0.9, 0.9, 0.9), b: Colour(0.2, 0.5, 0.2))
+horizontal = StripePattern(a: Colour(0.9, 0.9, 0.9), b: Colour(0.2, 0.5, 0.2), transform: rotation_y(Math::PI / 2))
+blended = BlendedPattern(a: horizontal, b: vertical)
+ring = RingPattern(a: Colour(0.8, 0.8, 0.8), b: Colour(0.8, 0.3, 0.3), transform: scaling(0.2, 0.2, 0.2) * translation(-25, -5, -5))
+checker = CheckerPattern(a: Colour(0.6, 0.6, 0.2), b: Colour(0.7, 0.7, 0.2))
+
 floor = Plane()
 floor.transform = translation(0, 0, 0)
-floor.material = Material().tap do |m|
-  #m.pattern = CheckerPattern(a: Colour(0.6, 0.6, 0.8), b: Colour(0.7, 0.7, 0.9))
-  m.pattern = RingPattern(a: Colour(0.6, 0.6, 0.8), b: Colour(0.7, 0.7, 0.9))
-  m.colour = Colour(1, 0.9, 0.9)
-  m.specular = 0
-end
+floor.material = Material(pattern: blended, specular: 0)
 
 back_wall = Plane()
 back_wall.transform =
   translation(0, 0, 3) *
   rotation_x(Math::PI / 2)
-back_wall.material = Material().tap do |m|
-  m.colour = Colour(1, 0.9, 0.9)
-  m.specular = 0
-end
+back_wall.material = Material(colour: Colour(0.7, 0.7, 0.7), specular: 0)
 
 left_wall = Plane()
 left_wall.transform =
   translation(0, 0, 5) *
   rotation_y(-Math::PI / 4) *
   rotation_x(Math::PI / 2)
-left_wall.material = Material().tap do |m|
-  m.colour = Colour(1, 0.9, 0.9)
-  m.pattern = BlendedPattern(
-    a: StripePattern(a: Colour::WHITE, b: Colour(0.3, 0.3, 0.7)).tap do |p|
-      p.transform = rotation_y(-Math::PI / 4)
-    end,
-    b: StripePattern(a: Colour::WHITE, b: Colour(0.3, 0.3, 0.7)).tap do |p|
-      p.transform = rotation_y(Math::PI / 4)
-    end
-  )
-  m.specular = 0
-end
+left_wall.material = Material.new(pattern: ring)
 
 right_wall = Plane()
 right_wall.transform =
   translation(0, 0, 5) *
   rotation_y(Math::PI / 4) *
   rotation_x(Math::PI / 2)
-right_wall.material = Material().tap do |m|
-  m.colour = Colour(1, 0.9, 0.9)
-  m.specular = 0
-end
+right_wall.material = Material(specular: 0, pattern: checker)
 
 left_side = Plane()
 left_side.transform =
   translation(-4, 0, 0) *
   rotation_y(-Math::PI / 2) *
   rotation_x(Math::PI / 2)
-left_side.material = Material().tap do |m|
-  m.colour = Colour(1, 0.9, 0.9)
-  m.specular = 0
-end
+left_side.material = Material(specular: 0)
 
 right_side = Plane()
 right_side.transform =
   translation(4, 0, 0) *
   rotation_y(Math::PI / 2) *
   rotation_x(Math::PI / 2)
-right_side.material = Material().tap do |m|
-  m.colour = Colour(1, 0.9, 0.9)
-  m.specular = 0
-end
-
+right_side.material = Material(specular: 0)
 
 middle_sphere = Sphere()
 middle_sphere.transform = translation(-0.5, 1, 0.5)
 middle_sphere.material = Material().tap do |m|
   r, g, b = pick_colour, pick_colour, pick_colour
-  #r = 0.9; g = 0.4; b = 0.8
-  #m.colour = Colour(0.1, 1, 0.5)
   m.pattern = StripePattern(
-    a: Colour(r, g, b), b: Colour(1.0 - r, 1.0 - g, 1.0 - b)
-  ).tap do |p|
-    p.transform = scaling(0.25, 0.25, 0.25) * rotation_y(-Math::PI / 4)
-  end
+    a: Colour(r, g, b), b: Colour(1.0 - r, 1.0 - g, 1.0 - b),
+    transform: scaling(0.25, 0.25, 0.25) * rotation_z(-Math::PI / 4)
+  )
   m.colour = Colour(r, g, b)
   m.diffuse = 0.7
   m.specular = 0.3
@@ -120,31 +94,29 @@ right_sphere.material = Material().tap do |m|
 end
 
 left_sphere = Sphere()
-left_sphere.transform =
-  translation(-1.5, 0.33, -0.75) * scaling(0.33, 0.33, 0.33)
-left_sphere.material = Material().tap do |m|
-  m.colour = Colour(0.3, 0.3, 0.7)
-  m.diffuse = 0.9
-  m.specular = 0.9
-end
+left_sphere.transform = translation(-1.5, 0.33, -0.75) * scaling(0.33, 0.33, 0.33)
+left_sphere.material = Material(
+  colour: Colour(0.3, 0.3, 0.7),
+  diffuse: 0.9,
+  specular: 0.9
+)
 
 hover_sphere = Sphere()
-hover_sphere.transform =
-  translation(-0.2, 1.5, -0.85) * scaling(0.25, 0.25, 0.25)
-hover_sphere.material = Material().tap do |m|
-  m.colour = Colour(0.8, 0.8, 0.1)
-  m.diffuse = 0.7
-  m.specular = 0.3
-end
+hover_sphere.transform = translation(-0.2, 1.5, -0.85) * scaling(0.25, 0.25, 0.25)
+hover_sphere.material = Material(
+  colour: Colour(0.8, 0.8, 0.1),
+  diffuse: 0.7,
+  specular: 0.3
+)
 
 hover_sphere2 = Sphere()
-hover_sphere2.transform =
-  translation(1.7, 1.75, 0.65) * scaling(0.5, 0.5, 0.5)
+hover_sphere2.transform = translation(1.7, 1.75, 0.65) * scaling(0.5, 0.5, 0.5)
 hover_sphere2.material = Material().tap do |m|
   m.colour = Colour(0.1, 0.8, 0.8)
-  m.pattern = CheckerPattern(a: m.colour, b: Colour::WHITE).tap do |p|
-    p.transform = scaling(0.5, 0.5, 0.5) * rotation_x(Math::PI / 4)
-  end
+  m.pattern = CheckerPattern(
+    a: m.colour, b: Colour::WHITE,
+    transform: scaling(0.5, 0.5, 0.5) * rotation_x(Math::PI / 4)
+  )
   m.diffuse = 0.7
   m.specular = 0.3
 end
@@ -152,13 +124,13 @@ end
 eclipsed_sphere = Sphere()
 eclipsed_sphere.transform =
   translation(0.85, 0.25, 1.5) * scaling(0.25, 0.25, 0.25)
-eclipsed_sphere.material = Material().tap do |m|
-  m.colour = Colour(0.9, 0.9, 0.9)
-  m.diffuse = 0.9
-  m.specular = 0.3
-end
+eclipsed_sphere.material = Material(
+  colour: Colour(0.9, 0.9, 0.9),
+  diffuse: 0.9,
+  specular: 0.3
+)
 
-light_position = Point(-3, 7, -10)
+light_position = Point(-3, 5, -10)
 light_colour = Colour(1, 1, 1)
 light = PointLight(light_position, light_colour)
 
@@ -182,7 +154,6 @@ world = World(
 
 camera = Camera(width, height, Math::PI / 3)
 camera.transform = Matrix.view_transform(
-  #from: Point(0, 1.5, -5),
   from: Point(0, 2.5, -5),
   to: Point(0, 1, 0),
   up: Vector(0, 1, 0)
